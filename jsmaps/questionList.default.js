003232d4b7a8c941d736924cb4161753b6962cf9
WurmABot <160713154+WurmABot@users.noreply.github.com>
2024-02-21 09:43:11 +0100

    Add files via upload

---
+++ jsmaps/questionList.default.js
@@ -0,0 +1,25 @@
+const qaMapWhatIs = new Map([
+  ['bot', 'A bot is a program that is designed to perform specific tasks on a server.\nIt can be used to automaticly answer questions, or to perform other tasks. Bots can be used to automate tasks, such as sending messages 🙂‍↕️'],
+  ['donkey', 'Sometimes i mean my Master is a Donky - is a Joke!\nA Donkey is  an animal for ride with it or drag a cart. \nFor players, that play on a free acount: You can only ride this animal!\nThey count in Wurm Online to the animal group of the  Herbivore, grazer. You can tame, breed and groom Donkeys.\nIf you kill a donkey you can  butcher him with a Butchering knife. :wurmpedia'],
+  ['horse', 'A Horse is  an animal for ride with it or drag a cart. You can breed, groom, and tame them. If you kill a horse you can  butcher they with a Butchering knife. Its an animal of the Group Herbivore, Grazer. It gives multiple kinds of Horses in Wurm Online. Not to be confused with Hell Horses.'],
+  ['cow', 'Cow is a domestic animal you can find wandering around grass lands. She is the female Kind of an Bull.\nCows can used for milking, breeding, grooming, taming and ride.\nThey can produce Meat, Fat, Tail, Hoof, Eye, Hide and Bladder when Butchering it.\nThe cow is an animal of the group Herbivore, Grazer. They can spawn anywhere in the world. :wurmpedia'],
+  ['bull', 'A male kind of an Cow.\nBulls can\'t be milked, but breeding, grooming, taming and ride.\nThey can produce Meat, Fat, Tail, Hoof, Eye, Hide and Bladder when Butchering it.\nThe Bull is an animal of the Group Herbivore, Grazer. They can spawn anywhere in the world. :wurmpedia'],
+  ['hell hound', 'Hellhounds aren\'t Dogs.\n They are mutch more agressiver  and stronger than Dogs. The hell hound has an extremely large aggro range, estimated at 10-20 tiles. One should have at least 40 fight skill and 50QL+ armour/weapons to take one on with the possibility of only medium wounds. Hell hounds will walk in shallow water, but do not swim. Hell hounds do slashing damage from their claws and burn damage.\n You can tame them.\mIts an animal of the Group Carnivore in Wurm Online. :wurmpedia'],
+  ['dragon', 'Dragons or Dragon hitchlings apears sometimes in the wild.\nThey are agressive and stronger than Hell Hounds. They can swim. They count\'t as an unique creature.\nAs such they owns in Wurm Online there own animal Group: Dragons.'],
+   ['deed', 'Deeds are playermade Settlements. They can be found by the usage of an Deed Strake on a free Place, if you are interested you can ask the people in its local chat for join it. :wurmpedia'],
+  
+  ['wurmpedia','Its an Wikipedia for Wurm Online.'],
+  ['wurm online','Wurm Online is a Sandbox-MMPORG-OpenWorld-game. :woLink'],
+  ['deer',':wurmpedia'],
+  ['wolf',':wurmpedia'],
+  ['knife',':wurmpedia'],
+  ['butchering knife',':wurmpedia'],
+  ['digging',':wurmpedia'],
+  ['demon',':wurmpedia']
+  
+  
+  
+]);
+module.exports={
+  qaMapWhatIs
+}
