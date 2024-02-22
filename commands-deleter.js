19d4454b1676095dee006915ede74f5705aa5dce
Thironix <160713154+WurmABot@users.noreply.github.com>
2024-02-22 18:32:46 +0100

    Update commands-deleter.js

--- commands-deleter.js
+++ commands-deleter.js
@@ -2,8 +2,9 @@ const { REST, Routes } = require('discord.js');
 
 const token = process.env['DISCORD_TOKEN'];
 client.config = require("./bot-config/main.json");
-const clientId = config.CLIENT_ID;
-const guildId= config.GUILD_ID;
+
+const clientId = config.clientid;
+const guildId= config.TestingServerID;
 
 const rest = new REST().setToken(token);
 console.log('[Info] Start for deletion all commands..');
