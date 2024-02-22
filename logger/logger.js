f9697632aff572224f531716590176eb07569733
Thironix <160713154+WurmABot@users.noreply.github.com>
2024-02-23 00:31:07 +0100

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -1,8 +1,4 @@
 const pino = require('pino');
 //const logger = pino();
-const logger = pino(pino.destination({
-  dest: './logger/my-file', // omit for stdout
-  minLength: 1024, // Buffer before writing
-  sync: false // Asynchronous logging
-}))
+const logger = pino()
 module.exports = logger;
