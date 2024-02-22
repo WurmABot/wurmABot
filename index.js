5dd62d96745ffc3b5e2bafd9e88ed6528b8ad04f
Thironix <160713154+WurmABot@users.noreply.github.com>
2024-02-22 21:40:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const {logger}=require('./logger/logger.js');
 
 const token = process.env["DISCORD_TOKEN"] || client.config.DISCORD_TOKEN;
 
-const manager = new ShardingManager('./WurmABot.js', { token: token });
+const manager = new ShardingManager('./WurmABot.js', { 
+	execArgv: ['--trace-warnings'],
+	shardArgs: ['--ansi', '--color'],
+  token: token });
 
 manager.on('shardCreate', shard => logger.info(`Launched shard ${shard.id}`));
 
