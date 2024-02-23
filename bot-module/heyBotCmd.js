166de3d38af69db61a2d890c1b463e81a06ddb0e
Thironix <160713154+WurmABot@users.noreply.github.com>
2024-02-23 19:08:39 +0100

    Update heyBotCmd.js
    a bit updating the code

--- bot-module/heyBotCmd.js
+++ bot-module/heyBotCmd.js
@@ -1,15 +1,41 @@
 const { bold, hyperlink, blockQuote } = require('discord.js');
 const nlp = require('compromise');
+const fs =require('fs').promises;
 const ilog= require('../logger/logger.js');
 // const { capitalize } = require('../utils.js');
 //const coreMap = require("../data/coreMap.js");
+let gsData=new Map();
+let gObjects=new Map();
+let deeds = new Map();
+let users = new Map();
+let botSettings= new Map();
+async function loadData(what=null,subname=null) {
+    let myFile = "../data/";
+    if (what === "gsData") {
+        myFile += "known-WO-Gameservers.json";
+        try {
+            const data = await fs.readFile(myFile, 'utf8');
+            gsData = new Map(JSON.parse(data));
+            ilog.info("Data ["+myFile+"] loaded successfully!");
+        } catch (err) {
+            ilog.error("Error loading data: ["+myFile+"]", err);
+        }
+    } else {
+        myFile += "nullfile.json";
+    }
+    
+}
+
 
 function heyBot(todo,inMsg,inGuild) {
-	ilog.info('Hey Bot call, todo:'+todo+', inMsg: '+inMsg.content);
+    await loadData('gsData');
+	ilog.info('[Hey Bot] call, todo:'+todo+', inMsg: '+inMsg.content);
 	var what=inMsg.content.toLowerCase();
 	var todoN=what.replace('hey bot,','');
+    
     var doc=nlp(todoN);
     
+
 
 	const dout=doc.out('array');
   	const topics= doc.topics().out('array');
@@ -19,6 +45,76 @@ function heyBot(todo,inMsg,inGuild) {
 	const acronyms=doc.acronyms().out('array');
 	const conjunctions=doc.conjunctions().out('array');
 	var action = doc.verbs(0).normalize(); //doing what?
+    var actionator =action.conjugate()[0].Gerund
+    var action2 = doc.verbs(1).normalize(); //doing what?
+    var actionator2 =action.conjugate()[1].Gerund
+    
+    rMsg='My teacher didn\'t explain to me how to do it';
+    switch (actionator) {
+        case "asking":
+            break;
+        case "being":
+            break;
+        case "building":
+            break;
+        case "can":
+            break;
+        case "crafting":
+            break;
+        case "getting":
+            break;
+        case "going":
+            break;
+        case "hearing":
+            break;
+        case "knowing":
+            break;
+        case "learning":
+            break;
+        case "liking":
+            break;
+        case "looking":
+            break;
+        case "needing":
+            break;
+        case "paying":
+            break;
+        case "playing":
+            break;
+        case "reporting":
+            break;
+        case "seeing":
+            break;
+        case "sending":
+            break;
+        case "sleeping":
+            break;
+        case "showing":
+            break;
+        case "starting":
+            break;
+        case "staying":
+            break;
+        case "thinking":
+             break;
+        case "talking":
+             break;
+        case "telling":
+             break;
+        case "trying":
+             break;
+        case "using":
+             break;
+        case "wanting":
+             break;
+        case "watching":
+             break;
+        case "writing":
+             break;
+        default:
+            rMsg='My teacher didn\'t explain to me how to do it';
+        
+    }
 
 	  // Bot denkt nach...
   	inMsg.channel.send(" :robot: WurmABot thinks...").then(() => {
@@ -26,7 +122,7 @@ function heyBot(todo,inMsg,inGuild) {
     		setTimeout(() => {
 
 		var bMsg = 'your message contains the follow:\n';
-        bMsg +='todo-selector is: ' +action.conjugate()[0].Gerund +"\n"
+        bMsg +='todo-selector is: ' +actionator+" and " +actionator2+" \n"
         +"___ \n"
       	+ '- topics: '+topics.join(', ')+'\n'
 		+ '- adjectives: '+adjectives.join(', ')+'\n'
@@ -42,4 +138,3 @@ function heyBot(todo,inMsg,inGuild) {
   	});
 }
 module.exports = {heyBot};
-
