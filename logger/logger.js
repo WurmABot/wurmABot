4a37d4836203d92444ae0e6b5b5fcfa94056f997
Thironix <160713154+WurmABot@users.noreply.github.com>
2024-02-23 10:24:48 +0100

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -1,4 +1,9 @@
 const pino = require('pino');
 //const logger = pino();
-const logger = pino()
+const logger = pino(transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true
+    }
+  })
 module.exports = logger;
