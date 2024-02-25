1f6134578df5889527d6b5b7a551f500cc4e4140
Thironix <160713154+WurmABot@users.noreply.github.com>
2024-02-25 13:01:12 +0100

    Add files via upload

--- logger/logger.js
+++ logger/logger.js
@@ -1,4 +1,9 @@
-const pino = require('pino');
-const pretty = require('pino-pretty')
+if (usebrowser) {
+    alert('debug of this file isn\'t possible here.');
+}
+else {
+    const pino = require('pino');
+    const pretty = require('pino-pretty')
+}
 const logger = pino(pretty())
 module.exports = logger;
